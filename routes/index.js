5e99c3e0d2ed3d06fba439c71c4517b1816f7d5b
Ross L. Owen <rowen@byupathway.edu>
2025-03-07 22:10:50 -0700

    week1: connecting to mongo

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 ﻿const router = require('express').Router();
+
 router.get('/', (req, res) => {
     res.send('hello world');
+})
+
+router.get('/contacts', (req, res) => {
+
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 ﻿const express = require('express');
+const mongo = require('./data');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes/index'));
 
-app.listen(port, () => console.log(`Listening on port: ${port}`));
+mongo.init((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => console.log(`DB live. Listening on port: ${port}`));
+    }
+})
+
